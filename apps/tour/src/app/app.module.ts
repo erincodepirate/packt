80cb2dfc6b7c7c68200c296b6b2a50beee19fd2f
Erin Clark <erincodepirate@gmail.com>
2022-03-30 18:07:16 -0700

    Add visitor and admin components

--- apps/tour/src/app/app.module.ts
+++ apps/tour/src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
+const routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('@packt/admin').then(m =>
+      m.AdminModule)
+  },
+  {
+    path: 'tour', 
+    loadChildren: () => import('@packt/visitor').then(m => 
+      m.VisitorModule)
+  },
+  {
+    path: '', pathMatch: 'full', redirectTo: 'tour'
+  }
+]
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
